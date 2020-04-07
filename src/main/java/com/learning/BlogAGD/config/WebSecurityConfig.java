6bb55472adb95e3a370f9f2341cb29bd2b954cec
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-04-07 20:36:17 +0300

    Add web.ignoring for not redirect static resources

--- src/main/java/com/learning/BlogAGD/config/WebSecurityConfig.java
+++ src/main/java/com/learning/BlogAGD/config/WebSecurityConfig.java
@@ -45,5 +45,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/css/**");
+        web.ignoring().antMatchers("/img/**");
     }
 }

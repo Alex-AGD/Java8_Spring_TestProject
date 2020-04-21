137fcbe75fca08606f8ef92e2099bf8d1fae70ff
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-04-21 21:22:57 +0300

    add YA metrika test

--- src/test/java/com/learning/BlogAGD/LoginTest.java
+++ src/test/java/com/learning/BlogAGD/LoginTest.java
@@ -6,8 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @TestPropertySource("/application-test.properties") //for test
 /*@ActiveProfiles("dev") //для dev properties*/
 @AutoConfigureMockMvc
@@ -40,24 +39,25 @@ public class LoginTest {
     }
 
     @Test
-    public void loginTest()throws Exception{
+    public void loginTest() throws Exception {
         this.mockMvc.perform(get("/main"))
                 .andDo(print())
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("http://localhost/login"));
 
-        }
+    }
+
     @Test
-    public void correctLoginTest()throws Exception{
-        this.mockMvc.perform(formLogin().user("admin").password("123"))
+    public void correctLoginTest() throws Exception {
+        this.mockMvc.perform(formLogin().user("admin").password("qqq"))
                 .andDo(print())
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/"));
     }
 
     @Test
-    public void notCorrectLogin() throws Exception{
-        this.mockMvc.perform(post("/login").param("anon","324"))
+    public void notCorrectLogin() throws Exception {
+        this.mockMvc.perform(post("/login").param("anon", "324"))
                 .andDo(print())
                 .andExpect(status().isForbidden());
     }
---
+++ src/test/java/com/learning/BlogAGD/service/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.learning.BlogAGD.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void addUser() {
+    }
+}
